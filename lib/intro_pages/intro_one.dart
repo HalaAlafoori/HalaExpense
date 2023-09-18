d9f5ca33e147ee4b8f9e2727137da3d006cde0b1
Hala Alafoori <moha6.afoori@gmail.com>
2023-09-18 20:08:57 +0300

    initialize

---
+++ lib/intro_pages/intro_one.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class IntroOne extends StatelessWidget {
+  const IntroOne({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
